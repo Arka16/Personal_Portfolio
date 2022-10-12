5bf66158b63a8d09b49cb54eeb6bd652eeb6b571
Arka Pal <arka.pal.0521@gmail.com>
2022-10-12 01:53:01 -0700

    first commit

---
+++ react-portfolio/src/components/Home/index.js
@@ -0,0 +1,17 @@
+import './index.scss'
+
+const Home = () => {
+  return (
+    <div className="container home-page">
+      <div className="text-zone">
+        <h1> Hi, I'm  ARKA
+          <br/>
+        Software Engineer
+        </h1>
+        <h2> Machine Learning | Artificial Intelligence | FullStack Development</h2>
+      </div>
+    </div>
+  )
+}
+
+export default Home
---
+++ react-portfolio/src/components/Projects/index.js
@@ -0,0 +1,22 @@
+import './index.scss'
+const Projects = () => {
+    return (
+        <div className='container about-page'>
+            <div className='text-zone'>
+               <h1>
+               Projects</h1>
+               <div>
+               <h4> Tennis </h4>
+               <p>I have been playing tennis for 7 years. I have played in my High School Varisity Tennis Team for 4 years and played both singles and doubles. </p>
+               <h4> Guitar </h4>
+               <p> I have been playing Guitar for 10 years. I know how to play 4 different types of guitars, which include Hawaian Lap Steel Guitar, Electric, Acoustic, and Bass Guitars  </p>
+               <h4> Working Out </h4>
+               <p>  I currently developed a passion for working out. I have been working out consistently for an year, and try to go to the gym 3-5 times a week. Favorite excercises include bench press, tricep pushdowns, and lat pulldowns </p>
+               </div>
+             
+            </div>
+        </div>
+    )
+}
+
+export default Projects
