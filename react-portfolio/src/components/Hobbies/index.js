c8e0f6eecfcdf75068e69cb5d758120ecdbe6a5b
Arka Pal <arka.pal.0521@gmail.com>
2022-10-12 16:13:02 -0700

    Hobbies complete

--- react-portfolio/src/components/Hobbies/index.js
+++ react-portfolio/src/components/Hobbies/index.js
@@ -1,4 +1,7 @@
 import './index.scss'
+import Tennis from '../../assets/images/Arka_tennis.jpg'
+import Guitar from '../../assets/images/Arka_guitar.jpg'
+import Workout from '../../assets/images/workout.jpg'
 const Hobbies = () => {
     return (
         <div className='container about-page'>
@@ -6,12 +9,26 @@ const Hobbies = () => {
                <h1>
                Hobbies</h1>
                <div>
-               <h4> Tennis </h4>
+                <div className='tennis'>
+                <h4> Tennis </h4>
                <p>I have been playing tennis for 7 years. I have played in my High School Varisity Tennis Team for 4 years and played both singles and doubles. </p>
-               <h4> Guitar </h4>
+               <img className='hobbieimg1' src= {Tennis} alt=' tennis'/>
+                </div>
+                
+                <div className='guitar'>
+                <h4> Guitar </h4>
                <p> I have been playing Guitar for 10 years. I know how to play 4 different types of guitars, which include Hawaian Lap Steel Guitar, Electric, Acoustic, and Bass Guitars  </p>
+               <img className='hobbieimg2' src= {Guitar} alt=' guitar'/>    
+                </div>
+                <div className='workout'>
+               
+               
                <h4> Working Out </h4>
                <p>  I currently developed a passion for working out. I have been working out consistently for an year, and try to go to the gym 3-5 times a week. Favorite excercises include bench press, tricep pushdowns, and lat pulldowns </p>
+               <img className='hobbieimg3' src= {Workout} alt=' workout'/>
+               </div>
+
+
                </div>
              
             </div>
