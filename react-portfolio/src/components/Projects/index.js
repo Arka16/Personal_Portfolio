9c3623f0f027d8d8739fedcd2a9303a95f05b423
Arka Pal <arka.pal.0521@gmail.com>
2022-10-12 17:58:42 -0700

    projects done

--- react-portfolio/src/components/Projects/index.js
+++ react-portfolio/src/components/Projects/index.js
@@ -1,4 +1,7 @@
 import './index.scss'
+import Bitcoin from '../../assets/images/bitcoin.png'
+import KVS from '../../assets/images/kvs.jpg'
+import WaveStyled from '../../assets/images/wavestyled.png'
 const Projects = () => {
     return (
         <div className='container about-page'>
@@ -6,12 +9,30 @@ const Projects = () => {
                <h1>
                Projects</h1>
                <div>
-               <h4> Tennis </h4>
-               <p>I have been playing tennis for 7 years. I have played in my High School Varisity Tennis Team for 4 years and played both singles and doubles. </p>
-               <h4> Guitar </h4>
-               <p> I have been playing Guitar for 10 years. I know how to play 4 different types of guitars, which include Hawaian Lap Steel Guitar, Electric, Acoustic, and Bass Guitars  </p>
-               <h4> Working Out </h4>
-               <p>  I currently developed a passion for working out. I have been working out consistently for an year, and try to go to the gym 3-5 times a week. Favorite excercises include bench press, tricep pushdowns, and lat pulldowns </p>
+                <div className='tennis'>
+                <h4> WaveStyled </h4>
+               <p> Clothing reccomendation IOS app, designed to reccomend outfits to users based on their personalized wardrobe. User add individual clothing items, enter details of the item(such as weather worn, occasion, color, etc...) which then feeded in to the reccomendation model to output fits tailored
+                to the users preference.
+                 </p>
+               <img className='projimg1' src= {WaveStyled} alt=' tennis'/>
+                </div>
+                
+                <div className='guitar'>
+                <h4> Bitcoin Predictor </h4>
+               <p> Trained and designed an RNN model to forecast bitcoin prices/trends from 2014 to 2021.
+Utilized Pandas and MinMaxScaler to clean and scale BTC price data from Yahoo Finance.
+Employed several GRU layers and adjusted the model’s timesteps to achieve a mean-squared loss of 0.0014. </p>
+               <img className='projimg2' src= {Bitcoin} alt=' guitar'/>    
+                </div>
+                <div className='workout'>
+               
+               
+               <h4> Key Value Store </h4>
+               <p>  Created a distributed, sharded, and fault-tolerant key-value store that spans many Docker containers. Implements Paxos algorithm and Causal broadcast algorithm </p>
+               <img className='projimg3' src= {KVS} alt=' workout'/>
+               </div>
+
+
                </div>
              
             </div>
